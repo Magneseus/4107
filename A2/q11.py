371944a433372648ccb82c15f8ae58ab82d0078f
Magneseus <matt.bmayer@gmail.com>
2017-10-29 14:21:36 -0400

    finished transferring to matrix operations for backpropagation

--- A2/q11.py
+++ A2/q11.py
@@ -26,7 +26,7 @@ def sigmoid_deriv(x):
 
 # Softmax function
 def softmax(sums):
-	returnMat = np.zeros(shape=[len(sums)], dtype=np.float32)
+	returnMat = np.zeros(shape=[len(sums), 1], dtype=np.float32)
 	
 	# e^x / Sigma e^x
 	returnMat = np.exp(sums) / np.sum(np.exp(sums), keepdims=True)
@@ -44,9 +44,9 @@ class NeuralNet:
 		self.rand = random.Random(None)
 
 		# Define the matrices for the neuron outputs
-		self.input_nodes  = np.zeros(shape=[self.num_in], dtype=np.float32)
-		self.hidden_nodes = [np.zeros(shape=[self.num_hi], dtype=np.float32) for i in range(self.num_hl)]
-		self.output_nodes = np.zeros(shape=[self.num_out], dtype=np.float32)
+		self.input_nodes  = np.zeros(shape=[1, self.num_in], dtype=np.float32)
+		self.hidden_nodes = [np.zeros(shape=[1, self.num_hi], dtype=np.float32) for i in range(self.num_hl)]
+		self.output_nodes = np.zeros(shape=[1, self.num_out], dtype=np.float32)
 
 		# Define the matrices for the weights
 		self.ih_weights = np.zeros(shape=[self.num_in, self.num_hi], dtype=np.float32)
@@ -54,8 +54,8 @@ class NeuralNet:
 		self.ho_weights = np.zeros(shape=[self.num_hi, self.num_out], dtype=np.float32)
 
 		# Define the matrices for the biases
-		self.h_biases = [np.zeros(shape=[self.num_hi], dtype=np.float32) for i in range(self.num_hl)]
-		self.o_biases = np.zeros(shape=[self.num_out], dtype=np.float32)
+		self.h_biases = [np.zeros(shape=[1, self.num_hi], dtype=np.float32) for i in range(self.num_hl)]
+		self.o_biases = np.zeros(shape=[1, self.num_out], dtype=np.float32)
 
 		# Initialize weights to random values
 		for i in range(self.num_in):
@@ -74,31 +74,31 @@ class NeuralNet:
 		# Initialize biases to random values
 		for ind in range(self.num_hl):
 			for i in range(self.num_hi):
-				(self.h_biases[ind])[i] = 0.02 * self.rand.random() - 0.01
+				(self.h_biases[ind])[0, i] = 0.02 * self.rand.random() - 0.01
 
 		for i in range(self.num_out):
-			self.o_biases[i] = 0.02 * self.rand.random() - 0.01
+			self.o_biases[0, i] = 0.02 * self.rand.random() - 0.01
 
 	def meanSquaredError(self, data, target):
 		sumSquaredError = 0.0
 
-		targMat = [np.zeros(shape=[self.num_out], dtype=np.float32) for i in range(len(target))]
+		targMat = [np.zeros(shape=[1, self.num_out], dtype=np.float32) for i in range(len(target))]
 		for i in range(len(target)):
 			for j in range(self.num_out):
-				(targMat[i])[j] = 1.0 if target[i] == j else 0.0
+				(targMat[i])[0, j] = 1.0 if target[i] == j else 0.0
 
 		for i in range(len(data)):
 			guesses = self.feed_forward(data[i])
 		
 			for j in range(self.num_out):
-				err = targMat[i][j] - guesses[j]
+				err = targMat[i][0, j] - guesses[0, j]
 				sumSquaredError += err * err
 			
 		return sumSquaredError / len(data)
 
 	def feed_forward(self, input_vals):
 		for i in range(self.num_in):
-			self.input_nodes[i] = input_vals[i]
+			self.input_nodes[0, i] = input_vals[i]
 
 		self.hidden_nodes[0] = sigmoid(self.input_nodes.dot(self.ih_weights) + self.h_biases[0])
 
@@ -108,16 +108,16 @@ class NeuralNet:
 		self.output_nodes = softmax(self.hidden_nodes[self.num_hl-1].dot(self.ho_weights) + self.o_biases)
 
 		# return a copy of the results
-		resultCopy = np.zeros(shape=self.num_out, dtype=np.float32)
+		resultCopy = np.zeros(shape=[1, self.num_out], dtype=np.float32)
 		for i in range(self.num_out):
-			resultCopy[i] = self.output_nodes[i]
+			resultCopy[0, i] = self.output_nodes[0, i]
 
 		return resultCopy
 
 	def run(self, dataList, targetList, maxRuns, eps_learn, lam_decay):
 		# Correction Matrices
-		outputCorrections = np.zeros(shape=[self.num_out], dtype=np.float32)
-		hiddenCorrections = [np.zeros(shape=[self.num_hi], dtype=np.float32) for i in range(self.num_hl)]
+		outputCorrections = np.zeros(shape=[1, self.num_out], dtype=np.float32)
+		hiddenCorrections = [np.zeros(shape=[1, self.num_hi], dtype=np.float32) for i in range(self.num_hl)]
 
 		# Data matrices and indices
 		targMat = [np.zeros(shape=[self.num_out], dtype=np.float32) for i in range(len(targetList))]
@@ -139,9 +139,7 @@ class NeuralNet:
 			for ind2 in range(len(dataList)):
 				ind = indList[ind2]
 
-				if (n != 0 and n%10000 == 0):
-					print("doing training num: %d  (%s)   err: %.4f" % (n, datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S'), self.meanSquaredError(dataList, targetList)))
-				elif (n%1000 == 0):
+				if (n%1000 == 0):
 					print("doing training num: %d  (%s)" % (n, datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M:%S')))
 				n += 1
 
@@ -168,37 +166,31 @@ class NeuralNet:
 
 				### Weight Updates
 				# Output Weights
-				for i in range(self.num_hi):
-					for j in range(self.num_out):
-						self.ho_weights[i,j] += eps_learn * outputCorrections[j] * (self.hidden_nodes[self.num_hl-1])[i]
+				self.ho_weights += eps_learn * np.dot(self.hidden_nodes[self.num_hl-1].T, outputCorrections)
 
 				# Output Bias
-				for i in range(self.num_out):
-					self.o_biases[i] += eps_learn * outputCorrections[i] * 1.0 # Or should this be -1?
+				self.o_biases += eps_learn * np.sum(outputCorrections) * 1.0 # Or should this be -1?
 
 				# Hidden weights
 				for hInd in range(self.num_hl-1):
-					for i in range(self.num_hi):
-						for j in range(self.num_hi):
-							(self.hh_weights[hInd])[i,j] += eps_learn * (self.hidden_nodes[hInd])[i] * (hiddenCorrections[hInd+1])[j]
+					self.hh_weights[hInd] += eps_learn * self.hidden_nodes[hInd].T.dot(hiddenCorrections[hInd+1])
 
 				# Hidden bias
 				for hInd in range(self.num_hl-1):
-					for i in range(self.num_hi):
-						(self.h_biases[hInd]) += eps_learn * (hiddenCorrections[hInd])[i] * 1.0 # Or should this be -1?
+					self.h_biases[hInd] += eps_learn * np.sum(hiddenCorrections[hInd]) * 1.0 # Or should this be -1?
+
+				#import pdb; pdb.set_trace()
 
 				# Input weights
-				for i in range(self.num_in):
-					for j in range(self.num_hi):
-						self.ih_weights[i,j] += eps_learn * self.input_nodes[i] * (hiddenCorrections[0])[j]
+				self.ih_weights += eps_learn * self.input_nodes.T.dot(hiddenCorrections[0])
 
 				#print("End   bp: %d" % (time.time() * 1000))
 
 			runs += 1
 
 			if (runs % 1 == 0):
-				print("finished run: %d", runs)
+				print("finished run %d:   err: %.4f" % (runs, self.meanSquaredError(dataList, targetList)))
 
 
-nn = NeuralNet(784, 2, 10, 10)
+nn = NeuralNet(784, 2, 15, 10)
 nn.run(mnist.data, mnist.target, 10, 0.01, 0.01)
