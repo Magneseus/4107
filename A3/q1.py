3bb0ac802ec783d017226194181284adaa864f44
Magneseus <matt.bmayer@gmail.com>
2017-11-13 22:10:50 -0500

    Added, split, and binarized the mnist data.

--- A3/q1.py
+++ A3/q1.py
@@ -4,7 +4,14 @@ import tensorflow as tf
 import random
 from sklearn.datasets import fetch_mldata
 
-rnd = random.Random()
+# Get the MNIST data, if not already there
+DATA_DIR = "./"
+mnist = fetch_mldata('MNIST original', data_home=DATA_DIR)
+
+# Initiate the random generator
+rnd = random.Random(3)
+
+##### TEMP STUFF REMOVE LATER ######
 
 x = np.zeros([5,6])
 
@@ -16,12 +23,27 @@ x[4] = np.array([0., 1., 1., 0., 0., 0.])
 
 x2 = np.zeros([5,6])
 
-x2[0] = np.array([-1., -1., 1., -1., 1., -1.])
-x2[1] = np.array([1., 1., 1., 1., -1., -1.])
-x2[2] = np.array([1., -1., 1., 1., 1., -1.])
-x2[3] = np.array([-1., 1., -1., -1., -1., 1.])
-x2[4] = np.array([-1., 1., 1., -1., -1., -1.])
+x2[0] = np.array([-1., -1.,  1., -1.,  1., -1.])
+x2[1] = np.array([ 1.,  1.,  1.,  1., -1., -1.])
+x2[2] = np.array([ 1., -1.,  1.,  1.,  1., -1.])
+x2[3] = np.array([-1.,  1., -1., -1., -1.,  1.])
+x2[4] = np.array([-1.,  1.,  1., -1., -1., -1.])
 
+##### END OF TEMP STUFF TO REMOVE LATER ######
+
+def binarize(vals, thresh=1):
+	return np.array([(-1. if val < thresh else 1.) for val in vals])
+
+# Separate the mnist data into two arrays
+mnist_train_1 = np.array([mnist.data[i] for i in range(60000) if mnist.target[i] == 1.0])
+mnist_train_5 = np.array([mnist.data[i] for i in range(60000) if mnist.target[i] == 5.0])
+mnist_test_1  = np.array([mnist.data[i] for i in range(60000, 70000) if mnist.target[i] == 1.0])
+mnist_test_5  = np.array([mnist.data[i] for i in range(60000, 70000) if mnist.target[i] == 5.0])
+
+# We can store 0.185 * 784 ~= 144 samples
+# Store 72 from each set, chosen randomly
+mnist_train_subset = np.array([(binarize(mnist_train_1[i//2]) if i % 2 == 0 else binarize(mnist_train_5[i//2])) for i in range(144)])
+rnd.shuffle(mnist_train_subset)
 
 # Creates the initial weights for a set of training examples
 # Needs to be in a binary 1,0 format (see matrix: x)
@@ -106,4 +128,18 @@ def hopfield(weights, vec, cap=-1):
 
 		iterations += 1
 
+	print("iterations" + str(iterations))
 	return vector
+
+w = weight_training_neg(mnist_train_subset)
+out = hopfield(w, binarize(mnist_test_5[1]))
+
+def print_out_img(out_vec):
+	for i in range(28):
+		s = ""
+		for j in range(28):
+			s += '0' if out_vec[(i*28)+j] == -1 else '1'
+			s += " "
+		print(s)
+
+print_out_img(out)
