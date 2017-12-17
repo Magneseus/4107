d29581162f9d5f2d2c6604f22546b9584ba014fe
Magneseus <matt.bmayer@gmail.com>
2017-12-17 17:08:44 -0500

    More input changes.

--- Project/input.py
+++ Project/input.py
@@ -8,7 +8,7 @@ def add_tuple_to_lists(lists, dir, files):
 	for f in files:
 		if (os.path.isfile(os.path.join(dir, f))):
 			labels.append(os.path.basename(dir))
-			data.append(os.path.join(dir, f))
+			data.append(os.path.join(os.path.abspath(dir), f))
 
 def get_labels_and_files(lfw_dir):
 	labels = []
@@ -17,4 +17,25 @@ def get_labels_and_files(lfw_dir):
 
 	return labels, data
 
-labels, data = get_labels_and_files("./lfw")
+# https://www.tensorflow.org/programmers_guide/datasets#decoding_image_data_and_resizing_it
+#
+# Reads an image from a file, decodes it into a dense tensor, and resizes it
+# to a fixed shape.
+def _parse_function(filename, label):
+  image_string = tf.read_file(filename)
+  image_decoded = tf.image.decode_jpeg(image_string)
+  image_resized = tf.image.resize_images(image_decoded, [128, 128])
+  return image_resized, label
+
+def get_dataset(lfw_dir):
+	labels, data = get_labels_and_files(lfw_dir)
+
+	tf_labels = tf.constant(labels)
+	tf_data   = tf.constant(data)
+
+	dataset = tf.data.Dataset.from_tensor_slices((tf_data, tf_labels))
+	dataset = dataset.map(_parse_function)
+
+	return dataset
+
+dataset = get_dataset('./lfw')
