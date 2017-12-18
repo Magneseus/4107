7a22c96e0a2bd54f7d6bd035fe91b504cf5bc9aa
Magneseus <matt.bmayer@gmail.com>
2017-12-17 23:28:24 -0500

    Adding lookup table to labels

--- Project/input.py
+++ Project/input.py
@@ -14,9 +14,24 @@ def add_tuple_to_lists(lists, dir, files):
 def get_labels_and_files(lfw_dir):
 	labels = []
 	data   = []
+
 	os.path.walk(lfw_dir, add_tuple_to_lists, (labels,data))
 
-	return labels, data
+	label_lookup = {}
+	it = 0
+	dct = {}
+	
+	for i in range(len(labels)):
+		lbl = labels[i]
+
+		if not lbl in dct:
+			dct[lbl] = it
+			it += 1
+
+		labels[i] = dct[lbl]
+		label_lookup[labels[i]] = lbl
+
+	return labels, data, label_lookup
 
 # https://www.tensorflow.org/programmers_guide/datasets#decoding_image_data_and_resizing_it
 #
@@ -29,16 +44,15 @@ def _parse_function(filename, label):
   return image_resized, label
 
 def get_dataset(lfw_dir):
-	labels, data = get_labels_and_files(lfw_dir)
+	labels, data, label_lookup = get_labels_and_files(lfw_dir)
 	size = len(labels)
 
 	tf_labels = tf.constant(labels)
 	tf_data   = tf.constant(data)
-
 	dataset = tf.data.Dataset.from_tensor_slices((tf_data, tf_labels))
 	dataset = dataset.map(_parse_function)
 
-	return dataset, size
+	return dataset, size, labels, label_lookup
 
 def split_dataset(dataset, size, split_point=0.5, shuffle=True):
 	split_marker = np.floor(size * split_point)
@@ -53,5 +67,5 @@ def split_dataset(dataset, size, split_point=0.5, shuffle=True):
 	return (_data_1, split_marker), (_data_2, size - split_marker)
 
 def get_data():
-	dataset, size = get_dataset('./lfw')
-	return split_dataset(dataset, size, split_point=0.9)
+	dataset, size, labels, label_lookup = get_dataset('./lfw')
+	return split_dataset(dataset, size, split_point=0.9), label_lookup
